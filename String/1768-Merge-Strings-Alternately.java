eb4b813a2193b702b63789b9b3d2a604e32af818
Sooraj S M <94183826+soorajsm@users.noreply.github.com>
2024-08-11 13:05:55 +0530

    [1768] [Time Beats: 100.00%] [Memory Beats: 41.03%] - LeetPush

---
+++ String/1768-Merge-Strings-Alternately.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder sb=new StringBuilder();
+        int i=0,j=0;
+        while(i<word1.length() && j<word2.length())
+        {
+            sb.append(word1.charAt(i));
+            sb.append(word2.charAt(j));
+            i++;
+            j++;
+        }
+        if(i<word1.length())
+            sb.append(word1.substring(i,word1.length()));
+        else if(j<word2.length())
+            sb.append(word2.substring(j,word2.length()));
+        return sb.toString();
+    }
+}
