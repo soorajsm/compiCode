e9e843ab6ae3c69e4e3b718ece092b936cedba09
Sooraj S M <94183826+soorajsm@users.noreply.github.com>
2024-08-21 13:03:56 +0530

    [1652] [Time Beats: 100.00%] [Memory Beats: 70.32%] - LeetPush

--- slidingWindow/1652-Defuse-the-Bomb.java
+++ slidingWindow/1652-Defuse-the-Bomb.java
@@ -1,33 +1,28 @@
 class Solution {
     public int[] decrypt(int[] code, int k) {
-        if (k == 0) return new int[code.length];
-        
-        int n = code.length;
-        int ksum = 0;
-        int[] res = new int[n];
-        
-        // build k size sum.
-        if (k > 0) {
-            for (int i = 0; i < k; i++) ksum += code[i];
+        int a[]=new int[code.length];
+        if(k==0)
+            return a;
             
-        } else {
-            for (int i = n - 1; i >= (n + k); i--) ksum += code[i];
+        int n=code.length;
+        int start=1,end=k,sum=0;
+
+        if(k<0){
+            k=-k;
+            start=n-k;
+            end=n-1;
         }
-        
-        for (int i = 0; i < n; i++) {
-                int next = code[((n + i + k) % n)];
-                int curr = code [i];
-                
-                if (k > 0) {
-                    res[i] = ksum - curr + next;
-                    ksum = res[i];
-                }
-                else {
-                    res[i] = ksum;
-                    ksum = ksum + curr - next; 
-                }
-                
+        for(int i=start;i<=end;i++)
+            sum+=code[i];
+
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sum;
+            end=(end+1)%n;
+            sum=sum-code[start]+code[end];
+            start=(start+1)%n;
         }
-        return res;
+
+        return a;
     }
 }
