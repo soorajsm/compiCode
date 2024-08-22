ef3e7d1f205b656988251cc603305f1f78ca3836
Sooraj S M <94183826+soorajsm@users.noreply.github.com>
2024-08-22 23:27:19 +0530

    [1493] [Time Beats: 68.46%] [Memory Beats: 25.63%] - LeetPush

--- slidingWindow/1493-Longest-Subarray-of-1's-After-Deleting-One-Element.java
+++ slidingWindow/1493-Longest-Subarray-of-1's-After-Deleting-One-Element.java
@@ -1,14 +1,12 @@
 class Solution {
     public int longestSubarray(int[] a) {
         int end=0;
-        int zcount=0,ocount=0;
+        int zcount=0;
         int max=Integer.MIN_VALUE;
         for(int start=0;start<a.length;start++)
         {
             if(a[start]==0)
                 zcount++;
-            else
-                ocount++;
             while(zcount>1)
             {
                 if(a[end]==0)
