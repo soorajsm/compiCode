4fa07f7b2abaebd3e45ceb3fc336163602bc525b
Sooraj S M <94183826+soorajsm@users.noreply.github.com>
2024-08-28 20:34:39 +0530

    [153] [Time Beats: 100.00%] [Memory Beats: 59.53%] - LeetPush

--- Binary Search/153-Find-Minimum-in-Rotated-Sorted-Array.java
+++ Binary Search/153-Find-Minimum-in-Rotated-Sorted-Array.java
@@ -1,6 +1,22 @@
 class Solution {
-    public int findMin(int[] nums) {
-        Arrays.sort(nums);
-        return nums[0];
+    public int findMin(int[] a) {
+        int n=a.length-1;
+        int left=0,right=n;
+        if(n==0)
+            return a[0];
+
+        while(left<=right)
+        {
+            int mid=left+(right-left)/2;
+            if(right==mid && mid==left)
+                return a[mid];
+
+            else if(a[mid]<a[right])
+
+                right=mid;
+            else
+                left=mid+1;
+        }
+        return 0;
     }
 }
