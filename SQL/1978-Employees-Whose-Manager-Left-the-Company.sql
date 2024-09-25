25b8732d3caeae55f45f5c8a4019a3475e1bebf4
Sooraj S M <94183826+soorajsm@users.noreply.github.com>
2024-09-25 16:34:37 +0530

    [1978] [Time Beats: 98.91%] - LeetPush

--- SQL/1978-Employees-Whose-Manager-Left-the-Company.sql
+++ SQL/1978-Employees-Whose-Manager-Left-the-Company.sql
@@ -2,10 +2,6 @@
 
 select a.employee_id from Employees a
 where a.salary < 30000 and 
-a.manager_id in ( select b.manager_id
-                     from Employees b
-                     where b.manager_id is not null and 
-                     b.manager_id not in 
-                     (select employee_id from Employees))
+a.manager_id not in ( select employee_id from Employees)
 order by a.employee_id
 
