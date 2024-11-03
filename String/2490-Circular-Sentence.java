8e3a3e2b0694d53e79a37190a75d0fa876e431e1
Sooraj S M <94183826+soorajsm@users.noreply.github.com>
2024-11-03 12:33:37 +0530

    [2490] [Time Beats: 100.00%] [Memory Beats: 54.50%] - LeetPush

--- String/2490-Circular-Sentence.java
+++ String/2490-Circular-Sentence.java
@@ -1,26 +1,61 @@
-class Solution {
-    public boolean isCircularSentence(String sentence) {
-        String sen[]=sentence.split(" ");
-        int arrlen=sen.length;
-        if(sen[0].charAt(0)==sen[sen.length-1].charAt(sen[sen.length-1].length()-1))
-            {
-                if(arrlen==1)
-                    return true;
-                int j=1,i=0;
-                while(j<arrlen)
-                {
-                    char curr=sen[i].charAt(sen[i].length()-1);
-                    char next=sen[j].charAt(0);
-                    if(curr==next)
-                    {
-                        i++;
-                        j++;    
-                    }
-                    else return false;
-                }
-            }
-            else return false;
 
+class Solution {
+    public boolean isCircularSentence(String s) {
+        if(s.charAt(0)!=s.charAt(s.length()-1))
+            return false;
+        int index=s.indexOf(" ");
+        if(index==-1)
+            return true;
+        while(index!=-1)
+        {
+            if(s.charAt(index-1)!=s.charAt(index+1))
+                return false;
+            index=s.indexOf(" ",index+1);
+        }
         return true;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public boolean isCircularSentence(String sentence) {
+//         String sen[]=sentence.split(" ");
+//         int arrlen=sen.length;
+//         if(sen[0].charAt(0)==sen[sen.length-1].charAt(sen[sen.length-1].length()-1))
+//             {
+//                 if(arrlen==1)
+//                     return true;
+//                 int j=1,i=0;
+//                 while(j<arrlen)
+//                 {
+//                     char curr=sen[i].charAt(sen[i].length()-1);
+//                     char next=sen[j].charAt(0);
+//                     if(curr==next)
+//                     {
+//                         i++;
+//                         j++;    
+//                     }
+//                     else return false;
+//                 }
+//             }
+//             else return false;
+
+//         return true;
+//     }
+// }
