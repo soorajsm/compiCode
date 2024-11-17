389934dbb8df1bd676557b6fd4a020acec3ec264
Sooraj S M <94183826+soorajsm@users.noreply.github.com>
2024-11-17 12:52:17 +0530

    [33] [Time Beats: 100.00%] [Memory Beats: 83.73%] - LeetPush

--- Binary Search/33-Search-in-Rotated-Sorted-Array.java
+++ Binary Search/33-Search-in-Rotated-Sorted-Array.java
@@ -1,33 +1,95 @@
 class Solution {
     public int search(int[] a, int target) {
-        int n=a.length-1;
-        if(a[0]==target)
-            return 0;
-        if(a[n]==target)
-            return n;
-
-        int left=1,right=n-1;
-        while(left<=right)
-        {
-            int mid=left+(right-left)/2;
+       int low=0,high=a.length-1;
+       while(low<=high)
+       {
+            int mid=low+(high-low)/2;
             if(a[mid]==target)
                 return mid;
-            else if(a[mid]>=a[left])
+            else if(a[low]<=a[mid])
             {
-                if(a[left]<=target && target<a[mid])
-                    right=mid-1;
+                if(a[low]<=target && target<=a[mid])
+                    high=mid;
                 else
-                    left=mid+1;
+                    low=mid+1;
             }
             else
             {
-                if(a[mid]<target && target<=a[right])
-                    left=mid+1;
+                if(a[mid]<=target && target<=a[high])
+                    low=mid;
                 else
-                    right=mid-1;
+                    high=mid-1;
             }
-        }
-        return -1;
-
+       }
+       return -1;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  int n=a.length-1;
+//         if(a[0]==target)
+//             return 0;
+//         if(a[n]==target)
+//             return n;
+
+//         int left=1,right=n-1;
+//         while(left<=right)
+//         {
+//             int mid=left+(right-left)/2;
+//             if(a[mid]==target)
+//                 return mid;
+//             else if(a[mid]>=a[left])
+//             {
+//                 if(a[left]<=target && target<a[mid])
+//                     right=mid-1;
+//                 else
+//                     left=mid+1;
+//             }
+//             else
+//             {
+//                 if(a[mid]<target && target<=a[right])
+//                     left=mid+1;
+//                 else
+//                     right=mid-1;
+//             }
+//         }
+//         return -1;
