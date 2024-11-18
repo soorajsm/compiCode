2f78ec08e1a4aee4db6e67a12807017a9512e6b7
Sooraj S M <94183826+soorajsm@users.noreply.github.com>
2024-11-18 11:55:28 +0530

    [153] [Time Beats: 100.00%] [Memory Beats: 86.86%] - LeetPush

--- Binary Search/153-Find-Minimum-in-Rotated-Sorted-Array.java
+++ Binary Search/153-Find-Minimum-in-Rotated-Sorted-Array.java
@@ -1,22 +1,86 @@
 class Solution {
     public int findMin(int[] a) {
-        int n=a.length-1;
-        int left=0,right=n;
-        if(n==0)
-            return a[0];
-
-        while(left<=right)
+        int low=0,high=a.length-1;
+        int mid=0;
+        while(low<=high)
         {
-            int mid=left+(right-left)/2;
-            if(right==mid && mid==left)
+            mid=low+(high-low)/2;
+            if(high==mid)
                 return a[mid];
-
-            else if(a[mid]<a[right])
-
-                right=mid;
+            else if(a[high]<a[mid] && a[high]<a[low])
+                low=mid+1;
             else
-                left=mid+1;
+                high=mid;
         }
-        return 0;
+        return mid;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // int n=a.length-1;
+        // int left=0,right=n;
+        // if(n==0)
+        //     return a[0];
+
+        // while(left<=right)
+        // {
+        //     int mid=left+(right-left)/2;
+        //     if(right==mid && mid==left)
+        //         return a[mid];
+
+        //     else if(a[mid]<a[right])
+
+        //         right=mid;
+        //     else
+        //         left=mid+1;
+        // }
+        // return 0;
