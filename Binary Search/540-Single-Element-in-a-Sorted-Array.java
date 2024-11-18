2b7c475d600df6f8fba91c326668b2270261c304
Sooraj S M <94183826+soorajsm@users.noreply.github.com>
2024-11-18 18:42:29 +0530

    [540] [Time Beats: 100.00%] [Memory Beats: 70.50%] - LeetPush

--- Binary Search/540-Single-Element-in-a-Sorted-Array.java
+++ Binary Search/540-Single-Element-in-a-Sorted-Array.java
@@ -1,12 +1,43 @@
 class Solution {
-    public int singleNonDuplicate(int[] nums) {
-        int j=0,i=0;
-        for(i=0;i<nums.length-1;i+=2)
+    public int singleNonDuplicate(int[] a) {
+        int low=0,high=a.length-1;
+        while(low<=high)
         {
-            j=i+1;
-            if(nums[i]!=nums[j])
-                return nums[i];
+            int mid=low+(high-low)/2;
+            if(low==high)
+                return a[mid];
+            
+            if((mid & 1) == 0)
+            {
+                if(a[mid]==a[mid+1])
+                    low=mid+2;
+                else
+                    high=mid;
+            }
+            else
+            {
+                if(a[mid]==a[mid+1])
+                    high=mid-1;
+                else
+                    low=mid+1;
+            }
         }
-        return nums[i];
+        return -1;
     }
 }
+
+
+
+
+
+
+        // Using two pointer
+
+        // int j=0,i=0;
+        // for(i=0;i<nums.length-1;i+=2)
+        // {
+        //     j=i+1;
+        //     if(nums[i]!=nums[j])
+        //         return nums[i];
+        // }
+        // return nums[i]; 
