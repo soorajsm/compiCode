c394887defe5391896a237eaacad0ae463402999
Sooraj S M <94183826+soorajsm@users.noreply.github.com>
2024-11-29 16:55:36 +0530

    [328] [Time Beats: 100.00%] [Memory Beats: 28.69%] - LeetPush

--- LinkedList/328-Odd-Even-Linked-List.java
+++ LinkedList/328-Odd-Even-Linked-List.java
@@ -10,23 +10,76 @@
  */
 class Solution {
     public ListNode oddEvenList(ListNode head) {
+
         if(head==null || head.next==null)
             return head;
+        ListNode t1=head, t2=head.next, h2=head.next;
 
-        ListNode first=head;
-        ListNode second=head.next;
-        ListNode secHead=second;
-
-        while(second!=null && second.next!=null)
+        while(t2!=null && t2.next!=null)
         {
-
-                first.next=first.next.next;
-                first=first.next;
-
-                second.next=second.next.next;
-                second=second.next;
+            t1.next=t1.next.next;
+            t2.next=t2.next.next;
+            t1=t1.next;
+            t2=t2.next;
         }
-        first.next=secHead;
+        t1.next=h2;
         return head;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public ListNode oddEvenList(ListNode head) {
+//         if(head==null || head.next==null)
+//             return head;
+
+//         ListNode first=head;
+//         ListNode second=head.next;
+//         ListNode secHead=second;
+
+//         while(second!=null && second.next!=null)
+//         {
+
+//                 first.next=first.next.next;
+//                 first=first.next;
+
+//                 second.next=second.next.next;
+//                 second=second.next;
+//         }
+//         first.next=secHead;
+//         return head;
+//     }
+// }
