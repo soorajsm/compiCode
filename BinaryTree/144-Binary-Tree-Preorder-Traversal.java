ece61d30f775abbd0960fe31539d56463b6bb8b8
Sooraj S M <94183826+soorajsm@users.noreply.github.com>
2024-12-23 16:35:28 +0530

    [144] [Time Beats: 100.00%] [Memory Beats: 16.85%] - LeetPush

--- BinaryTree/144-Binary-Tree-Preorder-Traversal.java
+++ BinaryTree/144-Binary-Tree-Preorder-Traversal.java
@@ -14,15 +14,53 @@
  * }
  */
 class Solution {
-    List<Integer>list=new ArrayList<>();
     public List<Integer> preorderTraversal(TreeNode root) {
-        if(root==null)
-            return list;
-        
-        list.add(root.val);
-        preorderTraversal(root.left);
-        preorderTraversal(root.right);
-
+        List<Integer>list=new ArrayList<>();
+        if(root!=null)
+        {
+            Stack<TreeNode> s=new Stack<>();
+            s.push(root);
+            while(!s.isEmpty())
+            {
+                TreeNode node=s.pop();
+                list.add(node.val);
+                if(node.right!=null) s.push(node.right);
+                if(node.left!=null) s.push(node.left);
+            }
+        }
         return list;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     List<Integer>list=new ArrayList<>();
+//     public List<Integer> preorderTraversal(TreeNode root) {
+//         if(root!=null)
+//         {
+//             list.add(root.val);
+//             preorderTraversal(root.left);
+//             preorderTraversal(root.right);
+//         }
+//         return list;
+//     }
+// }
