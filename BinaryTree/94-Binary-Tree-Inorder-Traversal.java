ff79250eea92902e8f72ed9f6604f2ad9d17f673
Sooraj S M <94183826+soorajsm@users.noreply.github.com>
2024-12-23 18:36:49 +0530

    [94] [Time Beats: 100.00%] [Memory Beats: 28.68%] - LeetPush

--- BinaryTree/94-Binary-Tree-Inorder-Traversal.java
+++ BinaryTree/94-Binary-Tree-Inorder-Traversal.java
@@ -14,13 +14,66 @@
  * }
  */
 class Solution {
-    List<Integer> list=new ArrayList<Integer>();
     public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer>list=new ArrayList<>();
         if(root==null)
             return list;
-        inorderTraversal(root.left);
-        list.add(root.val);
-        inorderTraversal(root.right);
+        Stack<TreeNode>s=new Stack<>();
+        s.push(root);
+        while(!s.isEmpty())
+        {
+            TreeNode node=s.peek();
+            if(node.left!=null)
+            {
+                s.push(node.left);
+                node.left=null;
+            }
+            else
+            {
+                s.pop();
+                list.add(node.val);
+                if(node.right!=null)
+                {
+                    s.push(node.right);
+                    node.right=null;
+                }
+            }
+        }
         return list;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public List<Integer> inorderTraversal(TreeNode root) {
+//         List<Integer>list=new ArrayList<>();
+//         helper(root,list);
+//         return list;
+//     }
+
+//     public void helper(TreeNode root,List<Integer>list)
+//     {
+//         if(root!=null)
+//         {
+//             helper(root.left,list);
+//             list.add(root.val);
+//             helper(root.right,list);
+//         }
+//         return;
+//     }
+// }
