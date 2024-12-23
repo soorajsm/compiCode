47a3190d03b62526636d11a06253d8bb227c349c
Sooraj S M <94183826+soorajsm@users.noreply.github.com>
2024-12-23 16:03:23 +0530

    [102] [Time Beats: 100.00%] [Memory Beats: 7.13%] - LeetPush

--- BinaryTree/102-Binary-Tree-Level-Order-Traversal.java
+++ BinaryTree/102-Binary-Tree-Level-Order-Traversal.java
@@ -14,38 +14,90 @@
  * }
  */
 class Solution {
-    List<List<Integer>>list=new ArrayList<>();
-    Queue<TreeNode>q=new LinkedList<>();
     public List<List<Integer>> levelOrder(TreeNode root) {
-        if(root==null)
-            return list;
-        q.add(root);
-        q.add(null);
-        List<Integer>l1=new ArrayList<>();
-        while(!q.isEmpty())
-        {
-            TreeNode ele=q.remove();
-            if(ele==null)
-            {
-                list.add(l1);
-                if(q.isEmpty())
-                {    
-                    break;
-                }
-                else
-                {
-                    l1=new ArrayList<>();
-                    q.add(null);
-                }   
-            }
-            else
-            {
-                l1.add(ele.val);
-                if(ele.left!=null)  q.add(ele.left);
-                if(ele.right!=null) q.add(ele.right);
-
-            }
-        }
+        List<List<Integer>>list=new ArrayList<>();
+        if(root!=null)
+            helper(root,0,list);
         return list;
     }
+
+    public void helper(TreeNode root,int level,List<List<Integer>>  list)
+    {
+        if(level>list.size()-1)
+            list.add(new ArrayList<>());
+
+        list.get(level).add(root.val);
+
+        if(root.left!=null) helper(root.left,level+1,list);
+        if(root.right!=null) helper(root.right,level+1,list); 
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Using queue
+
+// class Solution {
+//     public List<List<Integer>> levelOrder(TreeNode root) {
+//         if(root==null)
+//             return new ArrayList<>();
+//         Queue<TreeNode>q=new LinkedList<>();
+//         q.add(root);
+//         q.add(null);
+//         List<List<Integer>> list=new ArrayList<>();
+//         List<Integer> l=new ArrayList<>();
+
+//         while(!q.isEmpty())
+//         {
+//             TreeNode ele=q.poll();
+//             if(ele==null)
+//             {
+//                 if(q.isEmpty())
+//                 {
+//                     list.add(l);
+//                     return list;
+//                 }
+//                 else
+//                 {
+//                     list.add(l);
+//                     q.add(null);
+//                     l=new ArrayList<>();
+//                 }
+//             }
+//             else
+//             {
+//                 l.add(ele.val);
+//                 if(ele.left!=null) q.add(ele.left);
+//                 if(ele.right!=null) q.add(ele.right);
+//             }
+
+//         }                                                                                                                   return list;                
+//     }
+// }
