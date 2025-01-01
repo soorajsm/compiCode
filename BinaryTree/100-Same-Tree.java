b6d73b024d6dc97e3f7f60074db5e41b1e939344
Sooraj S M <94183826+soorajsm@users.noreply.github.com>
2025-01-02 00:02:01 +0530

    [100] [Time Beats: 100.00%] [Memory Beats: 14.99%] - LeetPush

--- BinaryTree/100-Same-Tree.java
+++ BinaryTree/100-Same-Tree.java
@@ -15,19 +15,79 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        return helper(p,q);
+        return helper(p,q);   
     }
-
     public boolean helper(TreeNode p,TreeNode q)
     {
         if(p==null && q==null)
             return true;
         if(p==null || q==null)
             return false;
-        if(p.val!=q.val)
+        if(p.val==q.val)
+            return helper(p.left,q.left) && helper(p.right,q.right);
+        else
             return false;
-        
-        return helper(p.left,q.left) && helper(p.right,q.right);
-        
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public boolean isSameTree(TreeNode p, TreeNode q) {
+//         return helper(p,q);
+//     }
+
+//     public boolean helper(TreeNode p,TreeNode q)
+//     {
+//         if(p==null && q==null)
+//             return true;
+//         if(p==null || q==null)
+//             return false;
+//         if(p.val!=q.val)
+//             return false;
+        
+//         return helper(p.left,q.left) && helper(p.right,q.right);
+        
+//     }
+// }
