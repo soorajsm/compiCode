2839421ed669af51501b9f428946e14130d955b2
Sooraj S M <94183826+soorajsm@users.noreply.github.com>
2025-01-02 13:21:12 +0530

    [2559] [Time Beats: 98.18%] [Memory Beats: 63.33%] - LeetPush

--- Prefix Sum/2559-Count-Vowel-Strings-in-Ranges.java
+++ Prefix Sum/2559-Count-Vowel-Strings-in-Ranges.java
@@ -5,7 +5,7 @@ class Solution {
         int sum=0;
         for(int i=0;i<words.length;i++)
         {
-             if(words[i].matches("[aeiou]|^[aeiou].*[aeiou]$"))
+             if(isVowelString(words[i]))
              {
                 psum[i]=sum+1;
                 sum=psum[i];
@@ -20,5 +20,18 @@ class Solution {
             res[i]=(s==0)?psum[e]:psum[e]-psum[s-1]; 
         }
         return res;
+    }
+
+    public boolean isVowelString(String s)
+    {
+        if(isVowel(s.charAt(0)) && isVowel(s.charAt(s.length()-1)))
+            return true;
+        return false;
+    }
+    public boolean isVowel(char c)
+    {
+        if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
+            return true;
+        return false;
     }
 }
