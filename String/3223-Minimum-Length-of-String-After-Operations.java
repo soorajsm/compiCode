dc2913cb46a0897891dd0dd40f11b93c5f3c9ad6
Sooraj S M <94183826+soorajsm@users.noreply.github.com>
2025-01-13 10:54:12 +0530

    [3223] [Time Beats: 92.05%] [Memory Beats: 66.89%] - LeetPush

--- String/3223-Minimum-Length-of-String-After-Operations.java
+++ String/3223-Minimum-Length-of-String-After-Operations.java
@@ -1,3 +1,10 @@
+
+
+
+
+
+
+
 class Solution {
     public int minimumLength(String s) {
         int freq[]=new int[26];
@@ -18,3 +25,33 @@ class Solution {
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public int minimumLength(String s) {
+//         int freq[]=new int[26];
+//         int count=0;
+//         for(byte b:s.getBytes())
+//             freq[b-97]++;
+
+//         for(int i=0;i<26;i++)
+//         {
+//             if(freq[i]>=3)
+//                 count+=(((freq[i]-3)/2)+1)*2;
+//         }
+//         return s.length()-count;
+
+//     }
+// }
