8b16b5983d3107d817e826dd85302997e0c10669
Sooraj S M <94183826+soorajsm@users.noreply.github.com>
2025-02-02 13:10:44 +0530

    [1752] [Time Beats: 100.00%] [Memory Beats: 50.38%] - LeetPush

--- Array/1752-Check-if-Array-Is-Sorted-and-Rotated.java
+++ Array/1752-Check-if-Array-Is-Sorted-and-Rotated.java
@@ -1,24 +1,44 @@
 class Solution {
     public boolean check(int[] a) {
         int n=a.length;
-        int minind=0;
-        for(int i=n-1;i>0;i--)
+        int irregular=0;
+        for(int i=0;i<n;i++)
         {
-            if(a[i-1]>a[i])
-            {
-                minind=i;
-                break;
-            }
-        }
-        int i=minind;
-        int count=n;
-        while(count-->1)
-        {
-            i=i%n;
             if(a[i]>a[(i+1)%n])
-                return false;
-            i++;
+                irregular++;
         }
-        return true;
+        return (irregular>1)?false:true;
     }
 }
+
+
+
+
+
+// bruteforce
+
+// class Solution {
+//     public boolean check(int[] a) {
+//         int n=a.length;
+//         int minind=0;
+//         for(int i=n-1;i>0;i--)
+//         {
+//             if(a[i-1]>a[i])
+//             {
+//                 minind=i;
+//                 break;
+//             }
+//         }
+//         int i=minind;
+//         int count=n;
+//         while(count-->1)
+//         {
+//             i=i%n;
+//             if(a[i]>a[(i+1)%n])
+//                 return false;
+//             i++;
+//         }
+//         return true;
+//     }
+// }
+
