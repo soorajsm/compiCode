20f7d4becf5b354c71181ebe2af717d00aa784e2
Sooraj S M <94183826+soorajsm@users.noreply.github.com>
2025-02-04 12:31:04 +0530

    [509] [Time Beats: 100.00%] [Memory Beats: 78.87%] - LeetPush

--- Dynamic_Programming/509-Fibonacci-Number.java
+++ Dynamic_Programming/509-Fibonacci-Number.java
@@ -1,17 +1,15 @@
 class Solution {
-    public int fib(int n) {
-        int dp[]=new int[n+1];
-        Arrays.fill(dp,-1);
-        return helper(n,dp);
-    }
-    public int helper(int n,int dp[])
+    public int fib(int i) {
+    if(i<=1)
+        return i;
+    int prev2=0,prev1=1;
+    for(int j=2;j<=i;j++)
     {
-        if(n<=1)
-            return n;
-        if(dp[n]!=-1)
-            return dp[n];
-        dp[n]=helper(n-1,dp)+helper(n-2,dp);
-        return dp[n];
+            int curr = prev1+prev2;
+            prev2=prev1;
+            prev1=curr;
+    }
+    return prev1;
     }
 }
 
@@ -23,17 +21,30 @@ class Solution {
 
 
 
+// // Using Dynamic programming;
+
 // class Solution {
-//     public int fib(int i) {
-//     if(i<=1)
-//         return i;
-//     int a=0,b=1;
-//     for(int j=2;j<=i;j++)
-//     {
-//         int temp=b;
-//         b=b+a;
-//         a=temp;
+//     public int fib(int n) {
+//         int dp[]=new int[n+1];
+//         Arrays.fill(dp,-1);
+//         return helper(n,dp);
 //     }
-//     return b;
+//     public int helper(int n,int dp[])
+//     {
+//         if(n<=1)
+//             return n;
+//         if(dp[n]!=-1)
+//             return dp[n];
+//         dp[n]=helper(n-1,dp)+helper(n-2,dp);
+//         return dp[n];
 //     }
 // }
+
+
+
+
+
+
+
+
+
