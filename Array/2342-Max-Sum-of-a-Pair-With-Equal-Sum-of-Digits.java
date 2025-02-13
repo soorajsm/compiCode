94b2c91b74fe252997d3f0c5286dc88daded89b5
Sooraj S M <94183826+soorajsm@users.noreply.github.com>
2025-02-13 17:03:27 +0530

    [2342] [Time Beats: 90.76%] [Memory Beats: 89.34%] - LeetPush

--- Array/2342-Max-Sum-of-a-Pair-With-Equal-Sum-of-Digits.java
+++ Array/2342-Max-Sum-of-a-Pair-With-Equal-Sum-of-Digits.java
@@ -1,20 +1,20 @@
 class Solution {
     public int maximumSum(int[] a) {
-        Map<Integer,Integer>map=new HashMap<>();
+        int map[]=new int[100];
         int b[]=new int[a.length];
         int max=0;
         for(int i=a.length-1;i>=0;i--)
         {
             int key=findsum(i,a,b);
-            if(map.containsKey(key))
+            if(map[key]!=0)
             {
-                int val=map.get(key);
+                int val=map[key];
                 max=Math.max(a[i]+val,max);
                 if(a[i]>val)
-                    map.put(key,a[i]);
+                    map[key]=a[i];
             }
             else
-                map.put(key,a[i]);
+                    map[key]=a[i];
         }
         return (max==0)?-1:max;
     }
@@ -33,3 +33,43 @@ class Solution {
         return res;
     }
 }
+
+
+
+
+
+// class Solution {
+//     public int maximumSum(int[] a) {
+//         Map<Integer,Integer>map=new HashMap<>();
+//         int b[]=new int[a.length];
+//         int max=0;
+//         for(int i=a.length-1;i>=0;i--)
+//         {
+//             int key=findsum(i,a,b);
+//             if(map.containsKey(key))
+//             {
+//                 int val=map.get(key);
+//                 max=Math.max(a[i]+val,max);
+//                 if(a[i]>val)
+//                     map.put(key,a[i]);
+//             }
+//             else
+//                 map.put(key,a[i]);
+//         }
+//         return (max==0)?-1:max;
+//     }
+//     public int findsum(int i,int a[],int b[])
+//     {
+//         if(b[i]!=0)
+//             return b[i];
+//         int res=0;
+//         int num=a[i];
+//         while(num>0)
+//         {
+//             res+=num%10;
+//             num/=10;
+//         }
+//         b[i]=res;
+//         return res;
+//     }
+// }
